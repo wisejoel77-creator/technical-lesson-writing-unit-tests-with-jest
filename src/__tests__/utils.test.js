7ec9b6639010bddd4160c00d0ace57e68b86d93c
Joel Wise <joelwise.ndungu@student.moringaschool.com>
2026-04-02 23:30:57 +0300

    run tests in utilis.test.js file

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+import { pointsForWord } from "../utils";
+
+describe("pointsForWord", () => {
+  it("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+    
+    // Define test value
+    const word = "test";
+
+    // Pass test value as argument to pointsForWord()
+    const points = pointsForWord(word);
+
+    // Check that return value "points" is 7 
+    expect(points).toBe(7);
+  });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+// src/utils.js
+export function pointsForWord(word) {
+  let points = 0;
+  for (const char of word) {
+    if (["a", "e", "i", "o", "u"].includes(char)) {
+      points += 1;
+    } else {
+      points += 2;
+    }
+  }
+  return points;
+}
